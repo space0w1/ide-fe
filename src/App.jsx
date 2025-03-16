dafa5adf853d7b950f6e02df94cd536843cc3417
spaceowl99 <jetan42@gmail.com>
2025-03-16 11:57:27 +0800

    clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import {Box} from '@chakra-ui/react'
 import CodeEditor from './components/CodeEditor';
-import LanguageSelector from './components/LanguageSelector';
 
 function App() {
   return (
